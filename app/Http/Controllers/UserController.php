2196c007dc589aa7300349c8c9f4ca850db5c9bd
ApocalCrk <56026108+ApocalCrk@users.noreply.github.com>
2023-01-05 16:52:56 +0700

    commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,6 +23,7 @@ class UserController extends Controller
 {
 
     private $nullvalue;
+    private $user;
 
     public function __construct(){
         $this->middleware(function ($request, $next) {
